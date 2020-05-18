c86166412eee539035a140aa4f3a922fcb8a0488
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-05-18 13:50:45 +0300

    Refactor app and index

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-require('./src/app')
+import http from 'http'
+import app from './src/app'
+
+const PORT = 4000
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
