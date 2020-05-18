d43bafb546bd2bcd2991e936673b58d4f88af2ab
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-05-18 12:38:11 +0300

    Add event schema

---
+++ src/models/event.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+})
+
+export default mongoose.model('Event', eventSchema)
