c78b9165bb235530383192f7b7d619b4f61f330a
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-05-19 15:35:26 +0300

    Add config for app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import http from 'http'
 import app from './app'
+import config from './utils/config'
 
-const PORT = 4000
+const PORT = config.get('port')
 
 const server = http.createServer(app)
 
---
+++ src/utils/config.ts
@@ -0,0 +1,18 @@
+import convict from 'convict'
+const config = convict({
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+  },
+  port: {
+    doc: 'The port to bind.',
+    format: 'port',
+    default: 4000,
+    env: 'PORT',
+    arg: 'port',
+  },
+})
+
+export default config
